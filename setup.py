bf423821afcbc7658bba21a922905fcccac9c642
Allan <allanklp@gmail.com>
2019-05-28 21:22:51 +0100

    add pypi description

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='json2dynamo',
-    version='0.0.2',
+    version='0.0.3',
     author='Allan Kilpatrick',
     author_email='allanklp@gmail.com',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/allankp/json2dynamo',
     packages=['json2dynamo'],
     package_dir={'json2dynamo': 'json2dynamo'},
