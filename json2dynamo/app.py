24018505e69cc6d2a96ff04dcdf2a157d81b36c8
Allan Kilpatrick <dubnerr@gmail.com>
2019-10-18 19:42:56 +0100

    add bakefile and black

--- json2dynamo/app.py
+++ json2dynamo/app.py
@@ -7,4 +7,4 @@ from json2dynamo.util import JsonTransfromer
 @click.argument("outputpath", type=click.Path())
 def cli(inputpath, outputpath):
     JsonTransfromer().transform_json_files(inputpath, outputpath)
-    click.echo(click.style(f'Transformation completed: {outputpath}', fg='green'))
+    click.echo(click.style(f"Transformation completed: {outputpath}", fg="green"))
--- json2dynamo/util.py
+++ json2dynamo/util.py
@@ -3,21 +3,26 @@ from dynamodb_json import json_util as json
 
 
 class JsonTransfromer:
-
     def transform_json_files(self, input_path: str, output_path: str):
         for json_file in self.get_json_files(input_path):
-            with open(f'{input_path}/{json_file}', 'r') as file:
+            with open(f"{input_path}/{json_file}", "r") as file:
                 template_json = json.loads(file.read())
-                self.write_file_to_folder(json.dumps(template_json), output_path, json_file)
+                self.write_file_to_folder(
+                    json.dumps(template_json), output_path, json_file
+                )
 
     @staticmethod
     def write_file_to_folder(data: dict, outpath: str, filename: str) -> list:
         if not os.path.exists(outpath):
             os.makedirs(outpath)
-        with open(f'{outpath}/{filename}', 'w+') as file:
+        with open(f"{outpath}/{filename}", "w+") as file:
             file.write(data)
             file.close()
 
     @staticmethod
     def get_json_files(input_path: str):
-        return [pos_json for pos_json in os.listdir(input_path) if pos_json.endswith('.json')]
+        return [
+            pos_json
+            for pos_json in os.listdir(input_path)
+            if pos_json.endswith(".json")
+        ]
